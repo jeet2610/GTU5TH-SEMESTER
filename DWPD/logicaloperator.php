1f5f1e4f18c24335cbef0f87f924e53dad0b1e6a
jeet shah <jeetshah0@icloud.com>
2021-01-07 17:00:31 +0530

    Initial Commit

---
+++ DWPD/logicaloperator.php
@@ -0,0 +1,37 @@
+<?php
+    $a=10;
+    $b=15;
+    if($a==10 and $b==15)
+    {
+        echo"true","<br>";
+    }
+    else
+    {
+        echo"false""<br>";
+    }
+    if($a==10 && $b==15)
+    {
+        echo"true""<br>";
+    }
+    else
+    {
+        echo"false""<br>";
+    }
+    if($a==10 or $b==20)
+    {
+        echo"true""<br>";
+    }
+    else
+    {
+        echo"false""<br>";
+    }
+    if($a==20 || $b==15)
+    {
+        echo"true""<br>";
+    }
+    else
+    {
+        echo"false""<br>";
+    }
+
+?>
